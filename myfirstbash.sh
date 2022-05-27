8f939510bfa42d7245e63d1beaa35d920413cb69
Vigen <v.a.filipetyan@gmail.com>
2022-05-27 20:37:54 +0400

    myfirstbash.sh

--- myfirstbash.sh
+++ myfirstbash.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-AGE=16
-name=Vigen
 echo "What is your name:"
 read -p name
 echo "How old are you?"
 read -p AGE
-echo "Your name is $name and you are $AGE years old."
+name=Vigen
+AGE=23
+echo "Your name is name and you are AGE years old."
